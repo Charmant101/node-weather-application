98b546546f20b19402b87badb6a0b783a7f82de2
Oladele Omoarukhe <daylayomoarukhe@yahoo.com>
2019-08-01 14:35:59 +0100

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const express = require('express');
-const app = express();
+
 const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast')
+
+
+const app = express();
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const directoryPath =  path.join(__dirname, '../public');
@@ -92,6 +96,6 @@ app.get('*',(req,res) =>{
        errorMessage: 'Page not found' 
     })
 })
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is up on port 3000");
 });
